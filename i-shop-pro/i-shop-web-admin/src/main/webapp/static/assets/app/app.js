41692ef3a83b9dfbfdf0e0cf35d80d6a6e16ca2d
lillusory <xueduanli@163.com>
2019-03-26 22:47:12 +0800

    优化:封装Dropzone图片上传

--- i-shop-pro/i-shop-web-admin/src/main/webapp/static/assets/app/app.js
+++ i-shop-pro/i-shop-web-admin/src/main/webapp/static/assets/app/app.js
@@ -4,6 +4,25 @@ var App = function () {
     var _checkbox;
     //存放ID的数组
     var _idArray;
+
+    // 默认的 Dropzone 参数
+    var defaultDropzoneOpts = {
+        url: "",
+        paramName: "dropFile",
+        maxFiles: 1, // 一次性上传的文件数量上限
+        maxFilesize: 2, // 文件大小，单位：MB
+        acceptedFiles: ".jpg,.gif,.png,.jpeg", // 上传的类型
+        addRemoveLinks: true,
+        parallelUploads: 1, // 一次上传的文件数量
+        dictDefaultMessage: '拖动文件至此或者点击上传',
+        dictMaxFilesExceeded: "您最多只能上传 1 个文件！",
+        dictResponseError: '文件上传失败!',
+        dictInvalidFileType: "文件类型只能是*.jpg,*.gif,*.png,*.jpeg",
+        dictFallbackMessage: "浏览器不受支持",
+        dictFileTooBig: "文件过大上传文件最大支持",
+        dictRemoveLinks: "删除",
+        dictCancelUpload: "取消"
+    };
     /**
      * 查看用户详情
      * @param url
@@ -220,6 +239,18 @@ var App = function () {
         });
     };
     /**
+     * 初始化 Dropzone
+     * @param opts
+     */
+    var handlerInitDropzone = function (opts) {
+        // 关闭 Dropzone 的自动发现功能
+        Dropzone.autoDiscover = false;
+        // 继承默认参数 获取默认属性
+        $.extend(defaultDropzoneOpts, opts);
+        new Dropzone(defaultDropzoneOpts.id, defaultDropzoneOpts);
+    };
+
+    /**
      * 向外暴露的方法
      */
     return {
@@ -261,6 +292,13 @@ var App = function () {
          */
         showDetail: function (url) {
             handlerShowDetail(url);
+        },
+        /**
+         * 初始化 Dropzone
+         * @param opts
+         */
+        initDropzone: function(opts) {
+            handlerInitDropzone(opts);
         }
     }
 }();
